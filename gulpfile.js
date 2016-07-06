457e6cde7fab9c38588bad4d192cef52249d281b
hunger <hunger@jirengu.com>
2016-07-06 15:39:21 +0800

    auto commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var gulp = require('gulp'),
 	shell = require('gulp-shell');
 
 
-gulp.task('d', shell.task(['git add .', 'git commit -am "auto commit"', 'git push -u origin master']));
+gulp.task('default', shell.task(['git add .', 'git commit -am "auto commit"', 'git push -u origin master']));
 
 gulp.task('watch', function(){
-	gulp.watch('**/*.md', ['d']);
+	gulp.watch('**/*.md', ['default']);
 });
